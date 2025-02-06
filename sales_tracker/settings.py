d05844f26231b71c2d1e3263b47ad9d8e6ca5bd0
ZéMaria <duartemaf@gmail.com>
2025-02-06 16:48:23 -0300

    Alteração no SETTINGS.PY para resolver conflito de static files

--- sales_tracker/settings.py
+++ sales_tracker/settings.py
@@ -103,8 +103,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "sales_tracker/core/static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
